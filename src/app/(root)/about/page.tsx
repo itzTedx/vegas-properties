1a8f34540c754a5c8b06944bf0716726a46aa3bd
melwin <114172287+itzTedx@users.noreply.github.com>
2025-09-08 18:43:45 +0400

    feat: enhance AboutPage layout with new content, improved styling, and background integration for a more engaging user experience

--- src/app/(root)/about/page.tsx
+++ src/app/(root)/about/page.tsx
@@ -1,9 +1,33 @@
+import { TunnelBackground } from "@/assets/tunnel-background";
+
 export default function AboutPage() {
   return (
-    <main className="container space-y-20 pt-6 pb-12">
-      <div className="space-y-4">
-        <h1 className="font-bold text-2xl">About Us</h1>
-      </div>
+    <main className="container relative min-h-screen space-y-20 pt-6 pb-12">
+      <TunnelBackground className="absolute top-0 left-0 w-full" />
+      <header className="relative z-20 mx-auto max-w-prose space-y-4 pt-12 text-lg">
+        <span className="block w-full text-center font-serif text-3xl">About Us</span>
+        <h1 className="font-medium">Your trusted partner in Dubai real estate</h1>
+        <p>
+          At Vegas Properties, we don’t just sell properties - we help you{" "}
+          <span className="font-medium">find a place to call home</span>, grow your investments, and achieve your real
+          estate dreams in Dubai.
+        </p>
+        <h2 className="mt-12 font-serif text-3xl">Our Story.</h2>
+        <h3 className="font-medium">A Legacy of Trust and Excellence</h3>
+        <p>
+          Founded with a vision to transform Dubai’s real estate experience, Vegas Properties has grown into one of the
+          region’s most trusted real estate agencies.
+        </p>
+        <p>
+          With years of experience in buying, selling, and managing properties, we have helped countless homeowners,
+          investors, and developers navigate Dubai’s dynamic property market.
+        </p>
+        <p>
+          Our commitment is simple: to provide personalized, transparent, and result-driven services that make real
+          estate transactions seamless and stress-free. At Vegas Properties, every client is treated like a partner, and
+          every property is handled with care and expertise.
+        </p>
+      </header>
     </main>
   );
 }
---
+++ src/assets/tunnel-background.tsx
@@ -0,0 +1,232 @@
+export const TunnelBackground = (props: SVGProps) => {
+  return (
+    <svg {...props} fill="none" height="1036" viewBox="0 0 1512 1036" width="1512" xmlns="http://www.w3.org/2000/svg">
+      <g clipPath="url(#clip0_75_3550)">
+        <path
+          d="M172.725 -289.95C172.725 -309.745 172.726 -324.539 174.258 -335.922C175.788 -347.29 178.836 -355.179 184.873 -361.211C190.91 -367.241 198.806 -370.287 210.185 -371.815C221.578 -373.345 236.387 -373.347 256.2 -373.347H1257.9C1277.71 -373.347 1292.52 -373.345 1303.91 -371.815C1315.29 -370.287 1323.19 -367.241 1329.23 -361.211C1335.26 -355.179 1338.31 -347.29 1339.84 -335.922C1341.38 -324.539 1341.38 -309.745 1341.38 -289.95V301.692C1341.38 321.487 1341.38 336.281 1339.84 347.664C1338.31 359.032 1335.26 366.921 1329.23 372.953C1323.19 378.983 1315.29 382.029 1303.91 383.557C1292.52 385.087 1277.71 385.089 1257.9 385.089H256.2C236.387 385.089 221.578 385.087 210.185 383.557C198.806 382.029 190.91 378.983 184.873 372.953C178.836 366.921 175.788 359.032 174.258 347.664C172.726 336.281 172.725 321.487 172.725 301.692V-289.95Z"
+          opacity="0.6"
+          stroke="url(#paint0_linear_75_3550)"
+        />
+        <path
+          d="M284.025 -181.902C284.025 -201.697 284.026 -216.491 285.558 -227.874C287.088 -239.242 290.136 -247.131 296.172 -253.162C302.21 -259.193 310.106 -262.238 321.485 -263.767C332.878 -265.297 347.686 -265.298 367.5 -265.298H1146.6C1166.41 -265.298 1181.22 -265.297 1192.61 -263.767C1203.99 -262.238 1211.89 -259.193 1217.93 -253.162C1223.96 -247.131 1227.01 -239.242 1228.54 -227.874C1230.07 -216.491 1230.07 -201.697 1230.07 -181.902V193.644C1230.07 213.439 1230.07 228.233 1228.54 239.616C1227.01 250.984 1223.96 258.873 1217.93 264.904C1211.89 270.935 1203.99 273.981 1192.61 275.509C1181.22 277.039 1166.41 277.04 1146.6 277.04H367.5C347.686 277.04 332.878 277.039 321.485 275.509C310.106 273.981 302.21 270.935 296.172 264.904C290.136 258.873 287.088 250.984 285.558 239.616C284.026 228.233 284.025 213.439 284.025 193.644V-181.902Z"
+          opacity="0.8"
+          stroke="url(#paint1_linear_75_3550)"
+        />
+        <path
+          d="M946.05 11.0269L1831.12 -873.2"
+          stroke="url(#paint2_linear_75_3550)"
+          strokeOpacity="0.4"
+          strokeWidth="1.17104"
+        />
+        <path
+          d="M762.3 164.801L1171.66 -1016.91"
+          stroke="url(#paint3_linear_75_3550)"
+          strokeOpacity="0.4"
+          strokeWidth="1.17104"
+        />
+        <path
+          d="M749.564 164.801L340.2 -1016.91"
+          stroke="url(#paint4_linear_75_3550)"
+          strokeOpacity="0.4"
+          strokeWidth="1.17104"
+        />
+        <path
+          d="M946.05 10.0671L1831.12 894.289"
+          stroke="url(#paint5_linear_75_3550)"
+          strokeOpacity="0.4"
+          strokeWidth="1.17104"
+        />
+        <path
+          d="M565.862 11.0269L-319.2 -873.2"
+          stroke="url(#paint6_linear_75_3550)"
+          strokeOpacity="0.4"
+          strokeWidth="1.17104"
+        />
+        <path
+          d="M565.862 10.0671L-319.2 894.289"
+          stroke="url(#paint7_linear_75_3550)"
+          strokeOpacity="0.4"
+          strokeWidth="1.17104"
+        />
+        <path
+          d="M926.1 10.6813H1747.2"
+          stroke="url(#paint8_linear_75_3550)"
+          strokeOpacity="0.4"
+          strokeWidth="1.17104"
+        />
+        <path
+          d="M585.9 10.6818H-235.2"
+          stroke="url(#paint9_linear_75_3550)"
+          strokeOpacity="0.4"
+          strokeWidth="1.17104"
+        />
+        <path
+          d="M1318.35 -500.801H1372.35C1411.95 -500.801 1431.75 -500.801 1444.04 -488.511C1456.35 -476.222 1456.35 -456.441 1456.35 -416.88V81.6443M190.322 -500.801H141.75C102.152 -500.801 82.3531 -500.801 70.0515 -488.511C57.75 -476.222 57.75 -456.441 57.75 -416.88V97.135"
+          opacity="0.3"
+          stroke="url(#paint10_linear_75_3550)"
+        />
+        <path
+          d="M1226.48 -373.871H1257.9C1297.5 -373.871 1317.3 -373.871 1329.59 -361.581C1341.9 -349.292 1341.9 -329.51 1341.9 -289.95V112.522M283.075 -373.871H256.2C216.602 -373.871 196.803 -373.871 184.502 -361.581C172.2 -349.292 172.2 -329.51 172.2 -289.95V125.458"
+          opacity="0.5"
+          stroke="url(#paint11_linear_75_3550)"
+        />
+        <path
+          d="M1137.15 -265.823H1146.6C1186.2 -265.823 1206 -265.823 1218.29 -253.533C1230.6 -241.243 1230.6 -221.463 1230.6 -181.902V128.605M373.275 -265.823H367.5C327.902 -265.823 308.103 -265.823 295.802 -253.533C283.5 -241.243 283.5 -221.463 283.5 -181.902V139.095"
+          stroke="url(#paint12_linear_75_3550)"
+        />
+        <path
+          d="M346.5 -117.912C346.5 -157.473 346.5 -177.253 358.802 -189.543C371.103 -201.833 390.902 -201.833 430.5 -201.833H1083.6C1123.2 -201.833 1143 -201.833 1155.29 -189.543C1167.6 -177.253 1167.6 -157.473 1167.6 -117.912V129.654C1167.6 169.215 1167.6 188.996 1155.29 201.285C1143 213.575 1123.2 213.575 1083.6 213.575H430.5C390.902 213.575 371.103 213.575 358.802 201.285C346.5 188.996 346.5 169.215 346.5 129.654V-117.912Z"
+          fill="#F5F6F8"
+        />
+      </g>
+      <defs>
+        <linearGradient
+          gradientUnits="userSpaceOnUse"
+          id="paint0_linear_75_3550"
+          x1="757.05"
+          x2="757.05"
+          y1="-373.347"
+          y2="385.089"
+        >
+          <stop stopColor="#7A86FF" />
+          <stop offset="1" stopColor="#E0E7FF" />
+        </linearGradient>
+        <linearGradient
+          gradientUnits="userSpaceOnUse"
+          id="paint1_linear_75_3550"
+          x1="757.05"
+          x2="757.05"
+          y1="-265.298"
+          y2="277.04"
+        >
+          <stop stopColor="#7A86FF" />
+          <stop offset="1" stopColor="#E0E7FF" />
+        </linearGradient>
+        <linearGradient
+          gradientUnits="userSpaceOnUse"
+          id="paint2_linear_75_3550"
+          x1="1451.45"
+          x2="1070.63"
+          y1="-492.281"
+          y2="-111.107"
+        >
+          <stop stopColor="#353FD7" stopOpacity="0" />
+          <stop offset="1" stopColor="#7A86FF" />
+        </linearGradient>
+        <linearGradient
+          gradientUnits="userSpaceOnUse"
+          id="paint3_linear_75_3550"
+          x1="996.763"
+          x2="820.756"
+          y1="-508.543"
+          y2="0.495633"
+        >
+          <stop stopColor="#353FD7" stopOpacity="0" />
+          <stop offset="1" stopColor="#7A86FF" />
+        </linearGradient>
+        <linearGradient
+          gradientUnits="userSpaceOnUse"
+          id="paint4_linear_75_3550"
+          x1="515.102"
+          x2="691.109"
+          y1="-508.543"
+          y2="0.495608"
+        >
+          <stop stopColor="#353FD7" stopOpacity="0" />
+          <stop offset="1" stopColor="#7A86FF" />
+        </linearGradient>
+        <linearGradient
+          gradientUnits="userSpaceOnUse"
+          id="paint5_linear_75_3550"
+          x1="1451.45"
+          x2="1070.63"
+          y1="513.375"
+          y2="132.201"
+        >
+          <stop stopColor="#353FD7" stopOpacity="0" />
+          <stop offset="1" stopColor="#7A86FF" />
+        </linearGradient>
+        <linearGradient
+          gradientUnits="userSpaceOnUse"
+          id="paint6_linear_75_3550"
+          x1="60.4672"
+          x2="441.282"
+          y1="-492.281"
+          y2="-111.107"
+        >
+          <stop stopColor="#353FD7" stopOpacity="0" />
+          <stop offset="1" stopColor="#7A86FF" />
+        </linearGradient>
+        <linearGradient
+          gradientUnits="userSpaceOnUse"
+          id="paint7_linear_75_3550"
+          x1="60.4672"
+          x2="441.283"
+          y1="513.375"
+          y2="132.201"
+        >
+          <stop stopColor="#353FD7" stopOpacity="0" />
+          <stop offset="1" stopColor="#5559FF" />
+        </linearGradient>
+        <linearGradient
+          gradientUnits="userSpaceOnUse"
+          id="paint8_linear_75_3550"
+          x1="-nan"
+          x2="-nan"
+          y1="-nan"
+          y2="-nan"
+        >
+          <stop stopColor="#353FD7" stopOpacity="0" />
+          <stop offset="1" stopColor="#CFFC75" />
+        </linearGradient>
+        <linearGradient
+          gradientUnits="userSpaceOnUse"
+          id="paint9_linear_75_3550"
+          x1="-nan"
+          x2="-nan"
+          y1="-nan"
+          y2="-nan"
+        >
+          <stop stopColor="#353FD7" stopOpacity="0" />
+          <stop offset="1" stopColor="#CFFC75" />
+        </linearGradient>
+        <linearGradient
+          gradientUnits="userSpaceOnUse"
+          id="paint10_linear_75_3550"
+          x1="757.05"
+          x2="757.05"
+          y1="-500.801"
+          y2="97.135"
+        >
+          <stop stopColor="#7A86FF" />
+          <stop offset="1" stopColor="#353FD7" stopOpacity="0" />
+        </linearGradient>
+        <linearGradient
+          gradientUnits="userSpaceOnUse"
+          id="paint11_linear_75_3550"
+          x1="757.05"
+          x2="757.05"
+          y1="-373.871"
+          y2="125.458"
+        >
+          <stop stopColor="#7A86FF" />
+          <stop offset="1" stopColor="#353FD7" stopOpacity="0" />
+        </linearGradient>
+        <linearGradient
+          gradientUnits="userSpaceOnUse"
+          id="paint12_linear_75_3550"
+          x1="757.05"
+          x2="757.05"
+          y1="-265.823"
+          y2="139.095"
+        >
+          <stop stopColor="#7A86FF" />
+          <stop offset="1" stopColor="#353FD7" stopOpacity="0" />
+        </linearGradient>
+        <clipPath id="clip0_75_3550">
+          <rect fill="white" height="1220" transform="translate(0 -184)" width="1512" />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
