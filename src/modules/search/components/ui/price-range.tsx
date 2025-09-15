c883554cd07118c1f92c4913e665a3fd09f87a3c
melwin <114172287+itzTedx@users.noreply.github.com>
2025-09-15 11:52:54 +0400

    feat: enhance PriceRange component by adding labels for minimum and maximum inputs, improving accessibility and user experience

--- src/modules/search/components/ui/price-range.tsx
+++ src/modules/search/components/ui/price-range.tsx
@@ -5,6 +5,7 @@ import { useEffect } from "react";
 import { Currency } from "@/components/ui/currency";
 import { FormDescription } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { Slider } from "@/components/ui/slider";
 
 import { useSliderWithInput } from "@/hooks/use-slider-with-input";
@@ -52,43 +53,49 @@ export default function PriceRange({ className, value, onChange, min, max }: Pro
 
   return (
     <div className={cn("flex flex-col gap-4", className)}>
-      <div className={cn("flex items-center gap-2")}>
-        <Input
-          aria-label="Enter minimum value"
-          className="h-8 w-16 px-2 py-1"
-          inputMode="decimal"
-          onBlur={() => validateAndUpdateValue(inputValues[0], 0)}
-          onChange={(e) => handleInputChange(e, 0)}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              validateAndUpdateValue(inputValues[0], 0);
-            }
-          }}
-          type="text"
-          value={inputValues[0]}
-        />
-        <Slider
-          aria-label="Dual range slider with input"
-          className="grow"
-          max={maxValue}
-          min={minValue}
-          onValueChange={handleSliderChange}
-          value={sliderValue}
-        />
-        <Input
-          aria-label="Enter maximum value"
-          className="h-8 w-16 px-2 py-1"
-          inputMode="decimal"
-          onBlur={() => validateAndUpdateValue(inputValues[1], 1)}
-          onChange={(e) => handleInputChange(e, 1)}
-          onKeyDown={(e) => {
-            if (e.key === "Enter") {
-              validateAndUpdateValue(inputValues[1], 1);
-            }
-          }}
-          type="text"
-          value={inputValues[1]}
-        />
+      <Slider
+        aria-label="Dual range slider with input"
+        className="grow"
+        max={maxValue}
+        min={minValue}
+        onValueChange={handleSliderChange}
+        value={sliderValue}
+      />
+      <div className="flex items-center gap-1">
+        <div>
+          <Label>Minimum</Label>
+          <Input
+            aria-label="Enter minimum value"
+            className="w-full px-2 py-1"
+            inputMode="decimal"
+            onBlur={() => validateAndUpdateValue(inputValues[0], 0)}
+            onChange={(e) => handleInputChange(e, 0)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                validateAndUpdateValue(inputValues[0], 0);
+              }
+            }}
+            type="text"
+            value={inputValues[0]}
+          />
+        </div>
+        <div>
+          <Label>Maximum</Label>
+          <Input
+            aria-label="Enter maximum value"
+            className="w-full px-2 py-1"
+            inputMode="decimal"
+            onBlur={() => validateAndUpdateValue(inputValues[1], 1)}
+            onChange={(e) => handleInputChange(e, 1)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                validateAndUpdateValue(inputValues[1], 1);
+              }
+            }}
+            type="text"
+            value={inputValues[1]}
+          />
+        </div>
       </div>
       <FormDescription>
         From <Currency />
