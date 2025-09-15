7c2fe6242ea366d86afccee4024479ddbf03837f
melwin <114172287+itzTedx@users.noreply.github.com>
2025-09-15 18:41:28 +0400

    feat: enhance AgentPage to display agent details with improved layout and conditional rendering for agent photo

--- src/app/(root)/agents/[slug]/page.tsx
+++ src/app/(root)/agents/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { notFound } from "next/navigation";
 
+import { ImageObject } from "@/lib/payload/components/media";
 import { getAgentBySlug } from "@/modules/agents/query";
 
 interface Props {
@@ -11,9 +12,21 @@ interface Props {
 export default async function AgentPage({ params }: Props) {
   const { slug } = await params;
   const agent = await getAgentBySlug(slug);
-  console.log("agents", agent);
-
   if (!agent) return notFound();
 
-  return <main>{JSON.stringify(agent, null, 2)}</main>;
+  return (
+    <main className="container max-w-7xl">
+      <div className="py-12">
+        <div>
+          <h1 className="font-serif text-4xl">{agent.name}</h1>
+          <p>{agent.title}</p>
+        </div>
+        {typeof agent.photo !== "number" && agent.photo && (
+          <div>
+            <ImageObject {...agent.photo} />
+          </div>
+        )}
+      </div>
+    </main>
+  );
 }
